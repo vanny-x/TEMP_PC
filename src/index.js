3259f608547ad4ecc5a82fc51d486f59d6f9ce45
维林 <489336723@qq.com>
2024-02-22 15:49:13 +0800

    vite script

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './routes/App.js';
 
-import './index.css'
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('App')).render(
   <React.StrictMode>
